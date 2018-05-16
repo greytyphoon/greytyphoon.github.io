9e57968f518f2ae26c990f8a14341d563b52dcbc
Rémi Doutre <remidoutre@gmail.com>
2018-05-16 07:45:38 -0400

    JSON'd Kashamy

---
+++ PF/archived/psychose/kashamy.js
@@ -0,0 +1,60 @@
+var Greytyphoon.Characters.push({
+    name: "Kashamy",
+    flair: "",
+    meta: {
+        quest: "Psychose",
+        owner: "Kashamy",
+        companion: { name: "Sissi L'Assoupi", link: "http://www.d20pfsrd.com/bestiary/monster-listings/animals/birds/dodo/" },
+        source: null,
+        dead: false,
+        npc: true,
+    },
+    alignment: "CG",
+    deity: "",
+    languages: "Common, Elven, Dwarven, Abyssal, Ignan, Terran",
+    ancestry: { 
+        name: "Elf", 
+        link: "http://www.d20pfsrd.com/races/core-races/elf",
+        archetypes: [ { title: "Arcane Focus", link: "http://www.d20pfsrd.com/races/core-races/elf#TOC-Arcane-Focus" } ]
+    },
+    level: [ { name: "Magus", link: "http://www.d20pfsrd.com/classes/base-classes/magus", quantity: 3 } ],
+    "str": 10,
+    "dex": [ 16, { points: 2, reason: "Racial" } ],
+    "con": [ 14, { points: -2, reason: "Racial" } ],
+    "int": [ 16, { points: 2, reason: "Racial" } ],
+    "wis": 10,
+    "cha": 10,
+    traits: [
+        { name: "Slippery", link: "http://www.d20pfsrd.com/traits/combat-traits/slippery-bellflower-network" },
+        { name: "Warrior of Old", link: "http://www.d20pfsrd.com/traits/race-traits/warrior-of-old" }
+    ],
+    startingFeats: [
+        { name: "Weapon Finesse", link: "http://www.d20pfsrd.com/feats/combat-feats/weapon-finesse-combat-final" }
+    ],
+    progressFeats: [
+        { name: "Fencing Grace", link: "http://www.d20pfsrd.com/feats/combat-feats/fencing-grace-combat" },
+        { name: "Familiar", link: "http://www.d20pfsrd.com/classes/base-classes/magus/magus-arcana/paizo---magus-arcana/familiar-ex" }
+    ],
+    targetFeats: [],
+    spells: [
+		{ level: 1, name: "blend", link: "http://www.d20pfsrd.com/magic/all-spells/b/blend" },
+		{ level: 1, name: "burning hands", link: "http://www.d20pfsrd.com/magic/all-spells/b/burning-hands" },
+		{ level: 1, name: "chill touch", link: "http://www.d20pfsrd.com/magic/all-spells/c/chill-touch", tag: "touch" },
+		{ level: 1, name: "frostbite", link: "http://www.d20pfsrd.com/magic/all-spells/f/frostbite", tag: "touch" },
+		{ level: 1, name: "magic weapon", link: "http://www.d20pfsrd.com/magic/all-spells/m/magic-weapon" },
+		{ level: 1, name: "mirror strike", link: "http://www.d20pfsrd.com/magic/all-spells/m/mirror-strike" },
+		{ level: 1, name: "obscuring mist", link: "http://www.d20pfsrd.com/magic/all-spells/o/obscuring-mist" },
+		{ level: 1, name: "shocking grasp", link: "http://www.d20pfsrd.com/magic/all-spells/s/shocking-grasp", tag: "touch" },
+		{ level: 1, name: "shock shield", link: "http://www.d20pfsrd.com/magic/all-spells/s/shock-shield" },
+		{ level: 1, name: "true strike", link: "http://www.d20pfsrd.com/magic/all-spells/t/true-strike" },
+		{ level: 1, name: "vanish", link: "http://www.d20pfsrd.com/magic/all-spells/v/vanish" }
+    ],
+    loot: [
+        { slot: "weapon-m", value: 300, name: "Rapier", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "weapon-r", value: 300, name: "Longbow [0]", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "armor", value: 150, name: "Studded Leather Armor", link: "http://www.d20pfsrd.com/equipment/armor" },
+        { slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
+        { slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" },
+        { slot: "none", value: 2200, name: "Handy Haversack", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/bag-handy-haversack" }
+    ]
+});
