c596a328aea8f99f9f84214b43ae03dc84944b93
Rémi Doutre <remidoutre@gmail.com>
2018-05-17 08:01:01 -0400

    JSON'd Supi

---
+++ PF/archived/psychose/supi.js
@@ -0,0 +1,51 @@
+var Greytyphoon.Characters.push({
+    name: "Supi",
+    flair: "",
+    meta: {
+        quest: "Supi",
+        owner: "Kashamy",
+        companion: { name: "Ligre", link: "http://www.d20pfsrd.com/classes/core-classes/druid/animal-companions#TOC-Cat-Big" },
+        source: null,
+        dead: false,
+        npc: false,
+    },
+    alignment: "NG",
+    deity: "",
+    languages: "Common, Elven, Dwarven, Celestial",
+    ancestry: { name: "Half-Elf",  link: "http://www.d20pfsrd.com/races/core-races/half-elf" },
+    level: [ { name: "Hunter", link: "http://www.d20pfsrd.com/classes/hybrid-classes/hunter", quantity: 3 } ],
+    "str": 10,
+    "dex": [ 16, { points: 2, reason: "Racial" } ],
+    "con": 14,
+    "int": 14,
+    "wis": 14,
+    "cha": 10,
+    traits: [
+        { name: "Heavenly Touch", link: "http://www.d20pfsrd.com/traits/regional-traits/heavenly-touch-tianjing" },
+		{ name: "Grim Optimism", link: "http://www.d20pfsrd.com/traits/social-traits/grim-optimi" }
+    ],
+    startingFeats: [
+        { name: "Skill Focus", link: "http://www.d20pfsrd.com/feats/general-feats/skill-focus-final" },
+		{ name: "Point-blank Shot", link: "http://www.d20pfsrd.com/feats/combat-feats/point-blank-shot-combat-final" }
+    ],
+    progressFeats: [
+        { name: "Precise Shot", link: "http://www.d20pfsrd.com/feats/combat-feats/precise-shot-combat-final" },
+		{ name: "Mounted Combat", link: "http://www.d20pfsrd.com/feats/combat-feats/mounted-combat-combat-final" },
+		{ name: "", link: "http://www.d20pfsrd.com/feats/combat-feats/coordinated-shot-combat-teamwork" }
+    ],
+    targetFeats: [],
+    spells: [
+		{ level: 1, name: "acid maw", link: "http://www.d20pfsrd.com/magic/all-spells/a/acid-maw" },
+		{ level: 1, name: "charm animal", link: "http://www.d20pfsrd.com/magic/all-spells/c/charm-animal" },
+		{ level: 1, name: "cure light wounds", link: "http://www.d20pfsrd.com/magic/all-spells/c/cure-light-wounds" },
+		{ level: 1, name: "expeditious excavation", link: "http://www.d20pfsrd.com/magic/all-spells/e/expeditious-excavation" },
+		{ level: 1, name: "summon nature's ally", link: "http://www.d20pfsrd.com/magic/all-spells/s/summon-natures-ally" }
+    ],
+    loot: [
+        { slot: "weapon-m", value: 300, name: "Greatsword", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "weapon-r", value: 300, name: "Longbow [0]", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "armor", value: 150, name: "Studded Leather Armor", link: "http://www.d20pfsrd.com/equipment/armor" },
+        { slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
+        { slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" }
+    ]
+});
