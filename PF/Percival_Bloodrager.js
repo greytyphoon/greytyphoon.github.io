68f3a5c82d1b8c5c189df0d9d2d9523fcc2b12b6
Rémi Doutre <remidoutre@gmail.com>
2018-05-22 18:30:45 -0400

    Quick notes on a Bloodrager.
    He'll be a new character in Dave's quest, probably.

---
+++ PF/Percival_Bloodrager.js
@@ -0,0 +1,53 @@
+var Greytyphoon.Characters.push({
+    name: "Percival Pendragon",
+    flair: "consumed by the black blood",
+    meta: {
+        quest: "Dave",
+        owner: "Grey",
+        companion: null,
+        source: null,
+        dead: false,
+        npc: false,
+    },
+    alignment: "N",
+    deity: "??",
+    languages: "Common",
+    ancestry: { name: "Human", link: "http://www.d20pfsrd.com/races/core-races/human" },
+    level: [ { 
+        name: "Bloodrager", 
+        link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager", 
+        quantity: 1, 
+        archetypes: [
+            { title: "Bloodline", name: "Black Blood", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/bloodrager-bloodlines/paizo---bloodrager-bloodlines/black-blood-bloodline" }
+        ] 
+    } ],
+    "str": [ 16, { points: 2, reason: "Racial" } ],
+    "dex": 14,
+    "con": 12,
+    "int": 10,
+    "wis": 12,
+    "cha": 11,
+    traits: [
+        { name: "??", link: "" },
+        { name: "Vengeful", link: "https://www.d20pfsrd.com/traits/combat-traits/vengeful/" }
+    ],
+    startingFeats: [
+        { name: "Power Attack", link: "http://www.d20pfsrd.com/feats/combat-feats/power-attack-combat-final" },
+        { name: "Quick Draw", link: "http://www.d20pfsrd.com/feats/combat-feats/quick-draw-combat-final" },
+    ],
+    progressFeats: [],
+    targetFeats: [
+        { name: "Raging Vitality", link: "http://www.d20pfsrd.com/feats/general-feats/raging-vitality" },
+        { name: "Arcane Strike", link: "http://www.d20pfsrd.com/feats/combat-feats/arcane-strike-combat" },
+        { name: "Blooded Arcane Strike", link: "http://www.d20pfsrd.com/feats/combat-feats/blooded-arcane-strike-combat" },
+        { name: "Combat Reflexes", link: "http://www.d20pfsrd.com/feats/combat-feats/combat-reflexes-combat" },
+        { name: "Bodyguard", link: "http://www.d20pfsrd.com/feats/combat-feats/bodyguard-combat" }
+    ],
+    spells: [],
+    loot: [
+        { slot: "weapon-m", value: 300, name: "Greatsword", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "weapon-m", value: 300, name: "Bardiche", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "weapon-r", value: 300, name: "Javelin", link: "http://www.d20pfsrd.com/equipment/weapons" },
+        { slot: "armor", value: 150, name: "Breastplate", link: "http://www.d20pfsrd.com/equipment/armor" }
+    ]
+});
