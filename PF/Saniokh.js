0243db194b58d032bd4a71c183cf6e26ac1d1f8d
Rémi Doutre <remidoutre@gmail.com>
2018-05-24 08:14:43 -0400

    Big commit

--- PF/Saniokh.js
+++ PF/Saniokh.js
@@ -1,5 +1,5 @@
 var Greytyphoon.Characters.push({
-    name: "Jocast Danmory",
+    name: "Saniokh Emishi",
     flair: "consumed by the black blood",
     meta: {
         quest: "Dave",
@@ -13,29 +13,30 @@ var Greytyphoon.Characters.push({
     deity: "Calistria",
     languages: "Common",
     ancestry: { name: "Human", link: "http://www.d20pfsrd.com/races/core-races/human" },
-    level: [ { 
-        name: "Bloodrager", 
-        link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager", 
-        quantity: 1, 
+    level: [ {
+        name: "Bloodrager",
+        link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager",
+        quantity: 1,
         archetypes: [
             { title: "Bloodline", name: "Black Blood", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/bloodrager-bloodlines/paizo---bloodrager-bloodlines/black-blood-bloodline" }
-        ] 
+        ]
     } ],
     "str": [ 16, { points: 2, reason: "Racial" } ],
-    "dex": 14,
-    "con": 13,
+    "dex": 12,
+    "con": 12,
     "int": 10,
     "wis": 10,
-    "cha": 12,
+    "cha": 11,
     traits: [
         { name: "Called", link: "http://www.d20pfsrd.com/traits/faith-traits/called" },
         { name: "Indomitable Faith", link: "http://www.d20pfsrd.com/traits/faith-traits/indomitable-faith" },
+        { name: "Carefully Hidden", link: "http://www.d20pfsrd.com/traits/race-traits/carefully-hidden-human" },
         { name: "Vengeful", link: "https://www.d20pfsrd.com/traits/combat-traits/vengeful/" },
         { name: "Warded against Nature", link: "https://www.d20pfsrd.com/traits/drawbacks/warded-against-nature/", drawback: true }
     ],
     startingFeats: [
         { name: "Power Attack", link: "http://www.d20pfsrd.com/feats/combat-feats/power-attack-combat-final" },
-        { name: "Quick Draw", link: "http://www.d20pfsrd.com/feats/combat-feats/quick-draw-combat-final" },
+        { name: "Quick Draw", link: "http://www.d20pfsrd.com/feats/combat-feats/quick-draw-combat-final" }
     ],
     progressFeats: [],
     targetFeats: [
