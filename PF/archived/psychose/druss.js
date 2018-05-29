141f22c93f4e758660d19975f2c84ddb247646f8
Rémi Doutre <remidoutre@gmail.com>
2018-05-29 07:49:34 -0400

    Little upgrades on model sheets.
    Removed "var" on line 1. Turned leading spaces to tabs, to save a few bytes.

--- PF/archived/psychose/druss.js
+++ PF/archived/psychose/druss.js
@@ -1,51 +1,51 @@
-var Greytyphoon.Characters.push({
-    name: "Druss Malroy",
-    flair: "",
-    meta: {
-        quest: "Psychose",
-        owner: "Cloj",
-        companion: null,
-        source: null,
-        dead: false,
-        npc: false,
-    },
-    alignment: "",
-    deity: "",
-    languages: "Common",
-    ancestry: { name: "Human", link: "http://www.d20pfsrd.com/races/core-races/human" },
-    level: [ { 
-        name: "Bloodrager", 
-        link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager", 
-        quantity: 3, 
-        archetypes: [
-            { title: "Bloodline", name: "Aberrant", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/bloodrager-bloodlines/paizo---bloodrager-bloodlines/aberrant" },
-            { name: "Greenrager", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/archetypes/paizo---bloodrager-archetypes/greenrager" }
-        ] 
-    } ],
-    "str": [ 17, { points: 2, reason: "Racial" } ],
-    "dex": 12,
-    "con": 14,
-    "int": 8,
-    "wis": 14,
-    "cha": 12,
-    traits: [
-        { name: "Helpful", link: "http://www.d20pfsrd.com/traits/combat-traits/helpful-combat" },
-        { name: "Honorable Champion", link: "http://www.d20pfsrd.com/traits/combat-traits/honorable-champion-combat" }
-    ],
-    startingFeats: [
-        { name: "Power Attack", link: "http://www.d20pfsrd.com/feats/combat-feats/power-attack-combat-final" },
-        { name: "Combat Reflexes", link: "http://www.d20pfsrd.com/feats/combat-feats/combat-reflexes-combat" }
-    ],
-    progressFeats: [
-        { name: "Bodyguard", link: "http://www.d20pfsrd.com/feats/combat-feats/bodyguard-combat" }
-    ],
-    targetFeats: [],
-    spells: [],
-    loot: [
-        { slot: "weapon-m", value: 300, name: "Greataxe", link: "http://www.d20pfsrd.com/equipment/weapons" },
-        { slot: "weapon-r", value: 300, name: "Longbow [3]", link: "http://www.d20pfsrd.com/equipment/weapons" },
-        { slot: "armor", value: 150, name: "Chain Shirt", link: "http://www.d20pfsrd.com/equipment/armor" },
-        { slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
-        { slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" }
-    ]
+Greytyphoon.Characters.push({
+	name: "Druss Malroy",
+	flair: "",
+	meta: {
+		quest: "Psychose",
+		owner: "Cloj",
+		companion: null,
+		source: null,
+		dead: false,
+		npc: false,
+	},
+	alignment: "",
+	deity: "",
+	languages: "Common",
+	ancestry: { name: "Human", link: "http://www.d20pfsrd.com/races/core-races/human" },
+	level: [ {
+		name: "Bloodrager",
+		link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager",
+		quantity: 3,
+		archetypes: [
+			{ title: "Bloodline", name: "Aberrant", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/bloodrager-bloodlines/paizo---bloodrager-bloodlines/aberrant" },
+			{ name: "Greenrager", link: "http://www.d20pfsrd.com/classes/hybrid-classes/bloodrager/archetypes/paizo---bloodrager-archetypes/greenrager" }
+		]
+	} ],
+	"str": [ 17, { points: 2, reason: "Racial" } ],
+	"dex": 12,
+	"con": 14,
+	"int": 8,
+	"wis": 14,
+	"cha": 12,
+	traits: [
+		{ name: "Helpful", link: "http://www.d20pfsrd.com/traits/combat-traits/helpful-combat" },
+		{ name: "Honorable Champion", link: "http://www.d20pfsrd.com/traits/combat-traits/honorable-champion-combat" }
+	],
+	startingFeats: [
+		{ name: "Power Attack", link: "http://www.d20pfsrd.com/feats/combat-feats/power-attack-combat-final" },
+		{ name: "Combat Reflexes", link: "http://www.d20pfsrd.com/feats/combat-feats/combat-reflexes-combat" }
+	],
+	progressFeats: [
+		{ name: "Bodyguard", link: "http://www.d20pfsrd.com/feats/combat-feats/bodyguard-combat" }
+	],
+	targetFeats: [],
+	spells: [],
+	loot: [
+		{ slot: "weapon-m", value: 300, name: "Greataxe", link: "http://www.d20pfsrd.com/equipment/weapons" },
+		{ slot: "weapon-r", value: 300, name: "Longbow [3]", link: "http://www.d20pfsrd.com/equipment/weapons" },
+		{ slot: "armor", value: 150, name: "Chain Shirt", link: "http://www.d20pfsrd.com/equipment/armor" },
+		{ slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
+		{ slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" }
+	]
 });
--- PF/archived/psychose/kashamy.js
+++ PF/archived/psychose/kashamy.js
@@ -1,42 +1,42 @@
-var Greytyphoon.Characters.push({
-    name: "Kashamy",
-    flair: "",
-    meta: {
-        quest: "Psychose",
-        owner: "Kashamy",
-        companion: { name: "Sissi L'Assoupi", link: "http://www.d20pfsrd.com/bestiary/monster-listings/animals/birds/dodo/" },
-        source: null,
-        dead: false,
-        npc: false,
-    },
-    alignment: "CG",
-    deity: "",
-    languages: "Common, Elven, Dwarven, Abyssal, Ignan, Terran",
-    ancestry: {
-        name: "Elf",
-        link: "http://www.d20pfsrd.com/races/core-races/elf",
-        archetypes: [ { name: "Arcane Focus", link: "http://www.d20pfsrd.com/races/core-races/elf#TOC-Arcane-Focus" } ]
-    },
-    level: [ { name: "Magus", link: "http://www.d20pfsrd.com/classes/base-classes/magus", quantity: 3 } ],
-    "str": 10,
-    "dex": [ 16, { points: 2, reason: "Racial" } ],
-    "con": [ 14, { points: -2, reason: "Racial" } ],
-    "int": [ 16, { points: 2, reason: "Racial" } ],
-    "wis": 10,
-    "cha": 10,
-    traits: [
-        { name: "Slippery", link: "http://www.d20pfsrd.com/traits/combat-traits/slippery-bellflower-network" },
-        { name: "Warrior of Old", link: "http://www.d20pfsrd.com/traits/race-traits/warrior-of-old" }
-    ],
-    startingFeats: [
-        { name: "Weapon Finesse", link: "http://www.d20pfsrd.com/feats/combat-feats/weapon-finesse-combat-final" }
-    ],
-    progressFeats: [
-        { name: "Fencing Grace", link: "http://www.d20pfsrd.com/feats/combat-feats/fencing-grace-combat" },
-        { name: "Familiar", link: "http://www.d20pfsrd.com/classes/base-classes/magus/magus-arcana/paizo---magus-arcana/familiar-ex" }
-    ],
-    targetFeats: [],
-    spells: [
+Greytyphoon.Characters.push({
+	name: "Kashamy",
+	flair: "",
+	meta: {
+		quest: "Psychose",
+		owner: "Kashamy",
+		companion: { name: "Sissi L'Assoupi", link: "http://www.d20pfsrd.com/bestiary/monster-listings/animals/birds/dodo/" },
+		source: null,
+		dead: false,
+		npc: false,
+	},
+	alignment: "CG",
+	deity: "",
+	languages: "Common, Elven, Dwarven, Abyssal, Ignan, Terran",
+	ancestry: {
+		name: "Elf",
+		link: "http://www.d20pfsrd.com/races/core-races/elf",
+		archetypes: [ { name: "Arcane Focus", link: "http://www.d20pfsrd.com/races/core-races/elf#TOC-Arcane-Focus" } ]
+	},
+	level: [ { name: "Magus", link: "http://www.d20pfsrd.com/classes/base-classes/magus", quantity: 3 } ],
+	"str": 10,
+	"dex": [ 16, { points: 2, reason: "Racial" } ],
+	"con": [ 14, { points: -2, reason: "Racial" } ],
+	"int": [ 16, { points: 2, reason: "Racial" } ],
+	"wis": 10,
+	"cha": 10,
+	traits: [
+		{ name: "Slippery", link: "http://www.d20pfsrd.com/traits/combat-traits/slippery-bellflower-network" },
+		{ name: "Warrior of Old", link: "http://www.d20pfsrd.com/traits/race-traits/warrior-of-old" }
+	],
+	startingFeats: [
+		{ name: "Weapon Finesse", link: "http://www.d20pfsrd.com/feats/combat-feats/weapon-finesse-combat-final" }
+	],
+	progressFeats: [
+		{ name: "Fencing Grace", link: "http://www.d20pfsrd.com/feats/combat-feats/fencing-grace-combat" },
+		{ name: "Familiar", link: "http://www.d20pfsrd.com/classes/base-classes/magus/magus-arcana/paizo---magus-arcana/familiar-ex" }
+	],
+	targetFeats: [],
+	spells: [
 		{ level: 1, name: "blend", link: "http://www.d20pfsrd.com/magic/all-spells/b/blend" },
 		{ level: 1, name: "burning hands", link: "http://www.d20pfsrd.com/magic/all-spells/b/burning-hands" },
 		{ level: 1, name: "chill touch", link: "http://www.d20pfsrd.com/magic/all-spells/c/chill-touch", tag: "touch" },
@@ -48,13 +48,13 @@ var Greytyphoon.Characters.push({
 		{ level: 1, name: "shock shield", link: "http://www.d20pfsrd.com/magic/all-spells/s/shock-shield" },
 		{ level: 1, name: "true strike", link: "http://www.d20pfsrd.com/magic/all-spells/t/true-strike" },
 		{ level: 1, name: "vanish", link: "http://www.d20pfsrd.com/magic/all-spells/v/vanish" }
-    ],
-    loot: [
-        { slot: "weapon-m", value: 300, name: "Rapier", link: "http://www.d20pfsrd.com/equipment/weapons" },
-        { slot: "weapon-r", value: 300, name: "Longbow [0]", link: "http://www.d20pfsrd.com/equipment/weapons" },
-        { slot: "armor", value: 150, name: "Studded Leather Armor", link: "http://www.d20pfsrd.com/equipment/armor" },
-        { slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
-        { slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" },
-        { slot: "none", value: 2200, name: "Handy Haversack", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/bag-handy-haversack" }
-    ]
+	],
+	loot: [
+		{ slot: "weapon-m", value: 300, name: "Rapier", link: "http://www.d20pfsrd.com/equipment/weapons" },
+		{ slot: "weapon-r", value: 300, name: "Longbow [0]", link: "http://www.d20pfsrd.com/equipment/weapons" },
+		{ slot: "armor", value: 150, name: "Studded Leather Armor", link: "http://www.d20pfsrd.com/equipment/armor" },
+		{ slot: "shoulders", value: 1000, name: "Cloak of Resistance", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/c-d/cloak-of-resistance" },
+		{ slot: "ring", value: 2000, name: "Ring of Protection", link: "http://www.d20pfsrd.com/magic-items/rings/ring-of-protection" },
+		{ slot: "none", value: 2200, name: "Handy Haversack", link: "http://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/bag-handy-haversack" }
+	]
 });
