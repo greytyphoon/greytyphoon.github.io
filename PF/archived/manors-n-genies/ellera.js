c3573bd6c5b2cda5ee373c65e84e7ce05438926e
Rémi Doutre <remidoutre@gmail.com>
2018-06-05 22:55:46 -0400

    Ellera fixes.

--- PF/archived/manors-n-genies/ellera.js
+++ PF/archived/manors-n-genies/ellera.js
@@ -14,25 +14,26 @@ Greytyphoon.Characters.push({
 		archetypes: [ { title: "Combat Style", name: "Archery", link: "https://www.d20pfsrd.com/classes/core-classes/ranger/ranger-combat-styles" } ]
 	} ],
 	"str": 16,
-	"dex": [ 16, { points: 2, reason: "Racial" }, { points: 1, reason: "Advancement" }, { points: 2, reason: "Enhancement" } ],
+	"dex": [ 16, { points: 2, reason: "Racial" }, { points: 2, reason: "Advancement" }, { points: 2, reason: "Enhancement" } ],
 	"con": 10,
 	"int": 10,
 	"wis": 14,
 	"cha": 10,
-	traits: [
-		{ name: "Fate's Favored", link: "https://www.d20pfsrd.com/traits/faith-traits/fate-s-favored" },
-		{ name: "Reactionary", link: "https://www.d20pfsrd.com/traits/combat-traits/reactionary" }
-	],
+	traits: [],
 	startingFeats: [
 		{ name: "Power Attack", reason: "Human", link: "https://www.d20pfsrd.com/feats/combat-feats/power-attack-combat-final" },
 		{ name: "Deadly Aim", reason: "Level 1", link: "https://www.d20pfsrd.com/feats/combat-feats/deadly-aim-combat" },
+		{ title: "Favored Enemy", name: "Undead", reason: "Ranger 1" }
 	],
 	progressFeats: [
 		{ name: "Rapid Shot", reason: "Ranger 2", link: "https://www.d20pfsrd.com/feats/combat-feats/rapid-shot-combat-final" },
 		{ name: "Quick Draw", reason: "Level 3", link: "https://www.d20pfsrd.com/feats/combat-feats/quick-draw-combat-final" },
 		{ name: "Endurance", reason: "Ranger 3", link: "https://www.d20pfsrd.com/feats/general-feats/endurance" },
+		{ title: "Favored Terrain", name: "Underground", reason: "Ranger 3" },
 		{ name: "Boon Companion", reason: "Level 5", link: "https://www.d20pfsrd.com/feats/general-feats/boon-companion" },
-		{ name: "Manyshot", reason: "Ranger 6", link: "https://www.d20pfsrd.com/feats/combat-feats/manyshot-combat-final" }
+		{ title: "2nd Favored Enemy", name: "Dragon", reason: "Ranger 5" },
+		{ name: "Manyshot", reason: "Ranger 6", link: "https://www.d20pfsrd.com/feats/combat-feats/manyshot-combat-final" },
+		{ title: "2nd Favored Terrain", name: "Forest", reason: "Ranger 8" }
 	],
 	targetFeats: [],
 	spells: [
@@ -45,7 +46,7 @@ Greytyphoon.Characters.push({
 	loot: [
 		{ slot: "weapon-m", value: 300, name: "Greasword, masterwork", link: "https://www.d20pfsrd.com/equipment/weapons" },
 		{ slot: "weapon-r", value: 8300, name: "Longbow [3], +1 Flaming", link: "https://www.d20pfsrd.com/magic-items/magic-weapons/magic-weapon-special-abilities/flaming" },
-		{ slot: "armor", value: 150, name: "Leather Armor", link: "https://www.d20pfsrd.com/equipment/armor" },
+        { slot: "armor", value: 1100, name: "Mithral Shirt", link: "https://www.d20pfsrd.com/magic-items/magic-armor/specific-magic-armor/mithral-shirt" },
 		{ slot: "neck", value: 16000, name: "Amulet of Shield", link: "https://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/bracers-of-armor" },
 		{ slot: "chest", value: 10000, name: "Bane Baldric", link: "https://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/baldric-bane" },
 		{ slot: "feet", value: 4000, name: "Belt of Incredible Dexterity", link: "https://www.d20pfsrd.com/magic-items/wondrous-items/wondrous-items/a-b/belt-of-incredible-dexterity" },
