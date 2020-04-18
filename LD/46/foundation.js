a41517f97801976fbffefb197495b9abbd31af65
Rémi Doutre <remidoutre@gmail.com>
2020-04-18 13:04:03 -0400

    LD46 Spawning viruses correctly

--- LD/46/foundation.js
+++ LD/46/foundation.js
@@ -20,9 +20,7 @@ function setupMaze()
 			let stationDiv = document.createElement("div");
 			stationDiv.classList.add("station");
 			stationDiv.style = "grid-column-start: " + (x*2) + "; "
-							 + "grid-row-start: " + (y*2) + "; "
-							 + "width: " + stationSize + "px; "
-							 + "height: " + stationSize + "px;";
+							 + "grid-row-start: " + (y*2) + ";";
 			gameBoard.appendChild(stationDiv);
 
 			let station = { positionX: x, positionY: y, dom: stationDiv, combines: null };
@@ -44,12 +42,12 @@ function spawnVirus()
 	virusesSpawned++;
 	let x = 0;
 	let y = 0;
-	let position = myRandom(gridSize);
+	let position = myRandom(gridSize) + 1; // random 1-10
 	switch (myRandom(4)) // 1:NORTH  2:EAST  3:SOUTH  0:WEST
 	{
 		case 0:
 			x = position;
-			y = 0;
+			y = 1;
 			break;
 		case 1:
 			x = 11;
@@ -60,7 +58,7 @@ function spawnVirus()
 			y = 11;
 			break;
 		case 3:
-			x = 0;
+			x = 1;
 			y = position + 1; // (Needs to enable (0,11)
 			break;
 	}
@@ -74,8 +72,8 @@ function spawnVirus()
 
 	let virusDom = document.createElement("strong");
 	virusDom.classList.add("virus");
-	virusDom.style = "grid-column-start: " + (x*2+1) + "; "
-				   + "grid-row-start: " + (y*2+1) + "; ";
+	virusDom.style = "grid-column-start: " + (x*2-1) + "; "
+				   + "grid-row-start: " + (y*2-1) + "; ";
 	gameBoard.appendChild(virusDom);
 
 	let virus = { positionX: x, positionY: y, dom: virusDom, potency: 1 };
